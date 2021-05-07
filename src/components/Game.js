4bd11617a06811c4e85205fd4c7420d6ac58d280
MaBen Studio <73801854+MajzikB@users.noreply.github.com>
2021-05-07 21:39:38 +0200

    Add files via upload

---
+++ src/components/Game.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
+  const xO = xIsNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    // return if won or occupied
+    if (winner || squares[i]) return;
+    // select square
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXisNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
+
+  const renderMoves = () =>
+    history.map((_step, move) => {
+      const destination = move ? `#${move} lépés` : "Játék újraindítása";
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    });
+
+  return (
+    <>
+      <h1>Tic Tac Toe játék - With React</h1>
+      <div className="info-wrapper">
+        <Board squares={history[stepNumber]} onClick={handleClick} />
+        <div className="box">
+          <h3>Kezdhetjük?</h3>
+          {renderMoves()}
+        </div>
+        <h3>{winner ? "A győztes: " + winner : "Következő játékos: " + xO}</h3>
+      </div>
+    </>
+  );
+};
+
+export default Game;
